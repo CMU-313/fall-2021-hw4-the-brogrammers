40129d5dc9b8884c560ce5058c4b9a1a912242a2
Yunsong Liu <leoyunsong@gmail.com>
2021-11-02 22:22:31 -0400

    change app

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -11,15 +11,24 @@ def hello():
 
 @app.route('/predict')
 def predict():
-	 #use entries from the query string here but could also use json
-     age = request.args.get('age')
-     absences = request.args.get('absences')
-     health = request.args.get('health')
-     data = [[age],[health],[absences]]
-     query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences)})
-     query = pd.get_dummies(query_df)
-     prediction = clf.predict(query)
-     return jsonify(np.asscalar(prediction))
+    include = ['school', 'age', 'traveltime', 'studytime', 'failures', 'schoolsup', 'famsup', 'higher', 'freetime', 'Dalc', 'Walc', 'health', 'absences']
+    vals = []
+    for f in features:
+        vals.append(request.args.get(f))
+    
+    query_df = pd.DataFrame(vals, columns=features)
+
+    with open("encoder", "rb") as f: 
+        encoder = pickle.load(f)
+    with open("categoricals", "rb") as f: 
+        categoricals = pickle.load(f)
+
+    query_cat = pd.DataFrame(encoder.transform(query_df[categoricals]).toarray(), columns=encoder.get_feature_names(categoricals))
+    query_val = query_df[query_df.columns.difference(categoricals)]
+    query = query_val.join(query_cat) # this order here is important
+
+    prediction = clf.predict(query)
+    return jsonify(np.asscalar(prediction))
 
 if __name__ == '__main__':
     clf = joblib.load('/apps/model2.pkl')
